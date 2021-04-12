8f5ee1a13ca5466a85a62d3535d4e3bb311a1e30
dorusd <dorusd@gmail.com>
2021-04-12 08:53:49 +0200

    die antwoord

--- Cipher/Program.cs
+++ Cipher/Program.cs
@@ -8,55 +8,82 @@ namespace Cipher
     {
         static void Main(string[] args)
         {
-            // We geven alle letters een cijfer. We beginnen van rechtst naar links, omdat dit ook de volgoorde is waarin we dit probleem willen oplossen
-            //   1 7 5 1 1
-            //   6 8 6 4 2
+            // We geven alle letters een ciifer. We beginnen van rechtst naar links, omdat dit ook de volgoorde is waarin we dit probleem willen oplossen
+            //   T A S T E
+            //   S I G H T+
+            // C I P H E R
+            //
+            //   1 7 5 1 2
+            //   5 8 6 4 1+
             //10 8 9 4 2 3
 
             Console.WriteLine("Hello World!");
 
-            // We proberen alle mogelijk getallen voor de eerste lekker en vervolgens voor de ander leters.
+            // We proberen alle mogeliik getallen voor de eerste lekker en vervolgens voor de ander leters.
 
-            int[] alles = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-            for (int i1 = 1; i1 < 9; i1++)
+            for (int i1 = 0; i1 < 10; i1++)
             {
-                int j1 = i1;
-                for (int i2 = 1; i2 < 8; i2++)
+                for (int i2 = 0; i2 < 10; i2++)
                 {
-                    int j2 = FindIndex(i2, j1);
-                    for (int i3 = 1; i3 < 8; i3++)
+                    if (IsBezet(i2, i1))
                     {
-                        int j3 = FindIndex(i3, j1, j2);
-                        if (Colomn1(j1, j2, j3))
+                        continue;
+                    }
+
+                    for (int i3 = 0; i3 < 10; i3++)
+                    {
+                        if (IsBezet(i3, i1, i2) || !Colomn1(i1, i2, i3))
                         {
-                            for (int i4 = 1; i4 < 7; i4++)
+                            continue;
+                        }
+
+                        for (int i4 = 0; i4 < 10; i4++)
+                        {
+                            if (IsBezet(i4, i1, i2, i3) ||! Colmn2(i1, i2, i3, i4))
                             {
-                                int j4 = FindIndex(i4, j1, j2, j3);
-                                if (Colmn2(j1, j2, j3, j4))
+                                continue;
+                            }
+
+                            for (int i5 = 0; i5 < 10; i5++)
+                            {
+                                if (IsBezet(i5, i1, i2, i3, i4))
                                 {
-                                    for (int i5 = 1; i5 < 6; i5++)
+                                    continue;
+                                }
+
+                                for (int i6 = 0; i6 < 10; i6++)
+                                {
+                                    if (IsBezet(i6, i1, i2, i3, i4, i5)|| !Colmn3(i1, i2, i3, i4, i5, i6))
                                     {
-                                        int j5 = FindIndex(i5, j1, j2, j3, j4);
-                                        for (int i6 = 1; i6 < 5; i6++)
+                                        continue;
+                                    }
+
+                                    for (int i7 = 0; i7 < 10; i7++)
+                                    {
+                                        if (IsBezet(i7, i1, i2, i3, i4, i5, i6))
                                         {
-                                            int j6 = FindIndex(i6, j1, j2, j3, j4, j5);
-                                            if (Colmn3(j1, j2, j3, j4, j5, j6))
+                                            continue;
+                                        }
+
+                                        for (int i8 = 0; i8 < 10; i8++)
+                                        {
+                                            if (IsBezet(i8, i1, i2, i3, i4, i5, i6, i7))
                                             {
-                                                for (int i7 = 1; i7 < 4; i7++)
+                                                continue;
+                                            }
+
+                                            for (int i9 = 0; i9 < 10; i9++)
+                                            {
+                                                if (IsBezet(i9, i1, i2, i3, i4, i5, i6, i7, i8))
                                                 {
-                                                    int j7 = FindIndex(i7, j1, j2, j3, j4, j5, j6);
-                                                    for (int i8 = 1; i8 < 3; i8++)
-                                                    {
-                                                        int j8 = FindIndex(i8, j1, j2, j3, j4, j5, j6, j7);
-                                                        int j9 = FindIndex(1, j1, j2, j3, j4, j5, j6, j7, j8);
-                                                        if (DeUitkomst(j1, j2, j3, j4, j5, j6, j7, j8, j9))
-                                                        {
-                                                            Console.WriteLine($"  {j1} {j7} {j5} {j1} {j1}");
-                                                            Console.WriteLine($"  {j6} {j8} {j6} {j4} {j2}");
-                                                            Console.WriteLine($"1 {j8} {j9} {j4} {j2} {j3}");
-                                                        }
-                                                    }
+                                                    continue;
+                                                }
+
+                                                if (DeUitkomst(i1, i2, i3, i4, i5, i6, i7, i8, i9))
+                                                {
+                                                    Console.WriteLine($"  {i1} {i7} {i5} {i1} {i2}");
+                                                    Console.WriteLine($"  {i5} {i8} {i6} {i4} {i1}");
+                                                    Console.WriteLine($"1 {i8} {i9} {i4} {i2} {i3}");
                                                 }
                                             }
                                         }
@@ -67,44 +94,38 @@ namespace Cipher
                     }
                 }
             }
+
             Console.WriteLine("geen uitkomst gevonden");
 
 
-            bool DeUitkomst(int j1, int j2, int j3, int j4, int j5, int j6, int j7, int j8, int j9)
+            bool DeUitkomst(int i1, int i2, int i3, int i4, int i5, int i6, int i7, int i8, int i9)
             {
-                return j1 + j2 + 10 * j1 + 10 * j4 + j5 * 100 + j6 * 100 
-                       + j7 + 1000 + j8 * 1000 + j1 * 10000 + j6 * 10000
-                       == j3 + 10 * j2 + j4 * 100 + j9 * 1000 + j8 * 10000 + 100000;
+                return i1 + i2 - i3 + 10 * (i1 + i4 - i2) + 100 * (i5 + i6 - i4) + 1000 * (i7 + i8 - i9) +
+                    10000 * (i1 + i5 - i8) + 100000 * (-1) == 0;
             }
 
-            bool Colmn3(int j1, int j2, int j3, int j4, int j5, int j6)
+            bool Colmn3(int i1, int i2, int i3, int i4, int i5, int i6)
             {
-                return j1 + j2 + 10 * j1 + 10 * j4 + j5 * 100 + j6 * 100 == j3 + 10 * j2 + j4 * 100 ||
-                       j1 + j2 + 10 * j1 + 10 * j4 + j5 * 100 + j6 * 100 == j3 + 10 * j2 + j4 * 100 + 100;
+                return i1 + i2 - i3 + 10 * (i1 + i4 - i2) + 100 * (i5 + i6 - i4) == 0 ||
+                       i1 + i2 - i3 + 10 * (i1 + i4 - i2) + 100 * (i5 + i6 - i4) == 1000;
             }
 
-            bool Colmn2(int j1, int j2, int j3, int j4)
+            bool Colmn2(int i1, int i2, int i3, int i4)
             {
-                return j1 + j2 + 10 * j1 + 10 * j4 == j3 + 10 * j2 ||
-                       j1 + j2 + 10 * j1 + 10 * j4 == j3 + 10 * j2 + 100;
+                return i1 + i2 - i3 + 10 * (i1 + i4 - i2) == 0 ||
+                       i1 + i2 - i3 + 10 * (i1 + i4 - i2) == 100;
             }
 
 
-            bool Colomn1(int j1, int j2, int j3)
+            bool Colomn1(int i1, int i2, int i3)
             {
-                return j1 + j2 == j3 ||
-                       j1 + j2 == j3 + 10;
+                return i1 + i2 - i3 == 0 ||
+                       i1 + i2 - i3 == 10;
             }
 
-            int FindIndex(int i, params int[] voorgangers)
+            bool IsBezet(int i, params int[] voorgangers)
             {
-                int index = i;
-                while (voorgangers.Contains(index))
-                {
-                    index += 1;
-                }
-
-                return index;
+                return i == 1 || voorgangers.Contains(i);
             }
         }
     }
